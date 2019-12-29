121b4b9c91d1df9616e0b7a42a3b92c052aeca65
tractors <huijianhua@163.com>
2019-12-29 17:48:40 +0800

    update feid and info

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -6,13 +6,23 @@
 #include "RecordBuffer.h"
 #include "AndroidLog.h"
 
+
+SL_API SLresult SLAPIENTRY slCreateEngine(
+        SLObjectItf             *pEngine,           //对象地址，用于传出对象
+        SLuint32                numOptions,         //配置参数数量
+        const SLEngineOption    *pEngineOptions,    //配置参数，为枚举数组
+        SLuint32                numInterfaces,      //支持的接口数量
+        const SLInterfaceID     *pInterfaceIds,     //具体的要支持的接口，是枚举的数组
+        const SLboolean         *pInterfaceRequired //具体的要支持的接口是开放的还是关闭的，也是一个数组，这三个参数长度是一致的
+);
+
 SLObjectItf  slObjectEngine = NULL;//用SLObjectltf声明引擎接口对象
 SLEngineItf engineItf = NULL;//声明具体的引擎对象实例
 SLObjectItf  recordObj = NULL;//用SLObjectltf声明引擎接口对象
 SLRecordItf recordItf = NULL;
-//SLEngineltf engineEngine = NULL;//声明具体的引擎对象实例
 
-SLAndroidSimpleBufferQueueItf  recorderBufferQueue = NULL;
+
+SLAndroidSimpleBufferQueueItf  recorderBufferQueue = NULL;//Buffer接口
 
 RecordBuffer *recordBuffer;
 
@@ -21,13 +31,19 @@ FILE *pcmFile = NULL;
 bool finish = false;
 //取出缓冲数据回调
 void bgRecorderCallback(SLAndroidSimpleBufferQueueItf bg, void *context){
-    fwrite(recordBuffer->getNowBuffer(),1,4096,pcmFile);
+    if (NULL != recordBuffer){
+        fwrite(recordBuffer->getNowBuffer(),1,4096,pcmFile);
+    }
+
     if (finish)
     {
         (*recordItf)->SetRecordState(recordItf,SL_RECORDSTATE_STOPPED);
-        //资源的释放
-        //销毁
-        (*slObjectEngine)->Destroy(slObjectEngine);
+        //刷新缓冲区后，关闭流
+        fclose(pcmFile);
+        //释放内存
+        delete  recordBuffer;
+        recordBuffer = NULL;
+
         LOGE("录制完成");
     } else {
         (*recorderBufferQueue)->Enqueue(recorderBufferQueue,recordBuffer->getRecordBuffer(),4096);
@@ -46,71 +62,74 @@ Java_com_will_openslesrecord_MainActivity_startRecord(JNIEnv *env, jobject insta
     recordBuffer = new RecordBuffer(4096);
     //第一步：创建引擎
     slCreateEngine(&slObjectEngine,0,NULL,0,NULL,NULL);
-    //第二步：实现(Realize)engineObject,SL_BOOLEAN_FALSE);
+    //第二步：实现(Realize)engineObject,SL_BOOLEAN_FALSE);实例化这个对象
     (*slObjectEngine)->Realize(slObjectEngine,SL_BOOLEAN_FALSE);
-    //第三步：通过engineObject的GetInterface方法初始化enngineEngine
+    //第三步：通过engineObject的GetInterface方法初始化enngineEngine,从这个对象里面获取引擎接口
     (*slObjectEngine)->GetInterface(slObjectEngine,SL_IID_ENGINE,&engineItf);
 
 
-    //设置混音器；
-//    const SLInterfaceID mids[1] = {SL_IID_ENVIRONMENTALREVERB};
-//    const SLboolean mreq[1] = {SL_BOOLEAN_FALSE};
-//    result = (*engineEngine)->CreateOutputMix(engineEngine, &outputMixObject, 1, mids, mreq);
-//    (void)result;
-//    result = (*outputMixObject)->Realize(outputMixObject, SL_BOOLEAN_FALSE);
-//    (void)result;
-//    result = (*outputMixObject)->GetInterface(outputMixObject, SL_IID_ENVIRONMENTALREVERB, &outputMixEnvironmentalReverb);
-//    if (SL_RESULT_SUCCESS == result) {
-//        result = (*outputMixEnvironmentalReverb)->SetEnvironmentalReverbProperties(
-//                outputMixEnvironmentalReverb, &reverbSettings);
-//        (void)result;
-//    }
-//    SLDataLocator_OutputMix outputMix = {SL_DATALOCATOR_OUTPUTMIX, outputMixObject};
 
-
+    //4. 设置IO设备(麦克风)
     SLDataLocator_IODevice loc_dev = {
-            SL_DATALOCATOR_IODEVICE,
-            SL_IODEVICE_AUDIOINPUT,
-            SL_DEFAULTDEVICEID_AUDIOINPUT,
-            NULL};
+            SL_DATALOCATOR_IODEVICE,//类型
+            SL_IODEVICE_AUDIOINPUT,//device类型 选择了音频输入类型
+            SL_DEFAULTDEVICEID_AUDIOINPUT,//deviceID
+            NULL            //device实例
+    };
 
-    SLDataSource audioStr = {&loc_dev,NULL};
-    // 创建播放器
+    SLDataSource audioStr = {
+            &loc_dev, //SLDataLocator_IODevice配置输入
+            NULL      //输入格式,采集的并不需要
+    };
+    //5. 设置输出buffer队列
     SLDataLocator_AndroidSimpleBufferQueue loc_bq = {
-            SL_DATALOCATOR_ANDROIDSIMPLEBUFFERQUEUE,
-            2,
+            SL_DATALOCATOR_ANDROIDSIMPLEBUFFERQUEUE,//类型 这里只能是这个常量
+            2,//buffer的数量
 
     };
-
+    //6. 设置输出数据的格式
     SLDataFormat_PCM format_pcm = {
-            SL_DATAFORMAT_PCM,//播放pcm格式的数据
-            2,//2个声道（立体声）
-            SL_SAMPLINGRATE_44_1,//44100hz的频率
-            SL_PCMSAMPLEFORMAT_FIXED_16,//位数 16位
-            SL_PCMSAMPLEFORMAT_FIXED_16,//和位数一致就行
-            SL_SPEAKER_FRONT_LEFT | SL_SPEAKER_FRONT_RIGHT,//立体声（前左前右）
-            SL_BYTEORDER_LITTLEENDIAN//结束标志
+            SL_DATAFORMAT_PCM,////输出PCM格式的数据
+            (SLuint32)2,//输出的声道数量
+            SL_SAMPLINGRATE_44_1,//输出的采样频率，这里是44100Hz
+            SL_PCMSAMPLEFORMAT_FIXED_16,//输出的采样格式，这里是16bit
+            SL_PCMSAMPLEFORMAT_FIXED_16,//一般来说，跟随上一个参数
+            SL_SPEAKER_FRONT_LEFT |
+            SL_SPEAKER_FRONT_RIGHT,//双声道配置，如果单声道可以用 SL_SPEAKER_FRONT_CENTER
+            SL_BYTEORDER_LITTLEENDIAN//PCM数据的大小端排列
     };
 
-    SLDataSink audioSink = {&loc_bq,&format_pcm};
+    SLDataSink audioSink = {
+            &loc_bq,     //SLDataFormat_PCM配置输出
+            &format_pcm  //输出数据格式
+    };
 
 
-
+    //7. 创建录制的对象
     const SLInterfaceID  id[1] = {SL_IID_ANDROIDSIMPLEBUFFERQUEUE};
     const SLboolean req[1] = {SL_BOOLEAN_TRUE};
 
     //创建录音器
-    (*engineItf)->CreateAudioRecorder(engineItf,&recordObj,&audioStr,&audioSink,1,id,req);
-    //实现
+    (*engineItf)->CreateAudioRecorder(
+            engineItf,  //引擎接口
+            &recordObj, //录制对象地址，用于传出对象
+            &audioStr,  //输入配置
+            &audioSink,  //输出配置
+            1,            //支持的接口数量
+            id,          //具体的要支持的接口
+            req          //具体的要支持的接口是开放的还是关闭的
+    );
+    ////8. 实例化这个录制对象
     (*recordObj)->Realize(recordObj,SL_BOOLEAN_FALSE);
+    //9. 获取录制接口
     (*recordObj)->GetInterface(recordObj,SL_IID_RECORD,&recordItf);
-
+    //10. 获取Buffer接口
     (*recordObj)->GetInterface(recordObj,SL_IID_ANDROIDSIMPLEBUFFERQUEUE,&recorderBufferQueue);
 
     (*recorderBufferQueue)->Enqueue(recorderBufferQueue,recordBuffer->getRecordBuffer(),4096);
 
     (*recorderBufferQueue)->RegisterCallback(recorderBufferQueue,bgRecorderCallback,NULL);
-
+    //11. 开始录音
     (*recordItf)->SetRecordState(recordItf,SL_RECORDSTATE_RECORDING);
 
     env->ReleaseStringUTFChars(path_, path);
@@ -120,6 +139,8 @@ extern "C"
 JNIEXPORT void JNICALL
 Java_com_will_openslesrecord_MainActivity_stopRecord(JNIEnv *env, jobject instance) {
 
-    finish = true;
+    if (NULL != recordItf) {
+        finish = true;
+    }
 
 }
